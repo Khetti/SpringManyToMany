3d1a056da903b996fc05cd6b0576518348c1b9aa
Greg Shanks <greg.shanks@gmail.com>
2020-02-11 18:49:47 +0000

    User class POJO complete

--- src/main/java/com/example/codeclan/fileservice/models/User.java
+++ src/main/java/com/example/codeclan/fileservice/models/User.java
@@ -1,4 +1,47 @@
 package com.example.codeclan.fileservice.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    private Long id;
+    private String name;
+    private List<Folder> folders;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
 }
