3585e08cfd8fb92922adfb0bf1a33f6447d12dca
Greg Shanks <greg.shanks@gmail.com>
2020-02-11 17:46:16 +0000

    FileController added, GET and POST tested via Insomnia

---
+++ src/main/java/com/example/codeclan/fileservice/controllers/FileController.java
@@ -0,0 +1,36 @@
+package com.example.codeclan.fileservice.controllers;
+
+import com.example.codeclan.fileservice.models.File;
+import com.example.codeclan.fileservice.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.xml.ws.Response;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class FileController {
+
+    @Autowired
+    private FileRepository fileRepository;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles() {
+        return new ResponseEntity<>(fileRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity<Optional<File>> getFile(@PathVariable Long id) {
+        return new ResponseEntity<>(fileRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> createFile(@RequestBody File file) {
+        fileRepository.save(file);
+        return new ResponseEntity<>(file, HttpStatus.CREATED);
+    }
+
+}
